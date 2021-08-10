a7a7e7e11a1d0a8e60641c296c6764f7fa9a3697
fatih <fatihenaz@gmail.com>
2021-08-10 13:03:15 +0300

    Initial commit

---
+++ tour-places/src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/navbar/Navbar"
+import "./App.css"
+import Header from "./components/header/Header";
+import Card from "./components/cards/Card"
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ tour-places/src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="container">
+            <ul className="navbar">
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">FOR YOU</a></li>
+                <li><a href="#">SERVİCES</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">VLOG</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
+
+
+
